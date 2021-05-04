6ca6a3efa3b3c681ea3850ce772bb86939ca0896
gbibbo <80938476+gbibbo@users.noreply.github.com>
2021-05-04 21:43:59 +0200

    Delete tivlib directory

--- tivlib/__init__.py
+++
@@ -1,8 +0,0 @@
-# Copyright (c) 2019 Antonio Ramires, Music Technology Grup, University Pompeu Fabra
-# This is an open-access library distributed under the terms of the Creative Commons Attribution 3.0 Unported License, which permits unrestricted use, distribution, and reproduction in any medium, provided the
-original author and source are credited.
-# Released under MIT License.
-
-from .version import __version__
-from .tiv import *
-
